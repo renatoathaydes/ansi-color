bb772da365b486b27349c394fbdbc750229a0e7a
renatoathaydes <renatoathaydes@hotmail.com>
2019-11-20 21:27:49 +0100

    Changed version to 0.2

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "A library to make it easy to colorize terminal output using ANSI escape sequences")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(renato-athaydes))
 (define scribblings '(("scribblings/ansi-color.scrbl" ())))
 (define clean '("compiled" "doc" "doc/ansi-color"))
